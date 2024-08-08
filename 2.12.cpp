81c2eafaeda032bed742d81b1c5eb3dfaff47580
User <User@DESKTOP-KGVD8QQ>
2024-08-08 16:06:28 +0300

    Добавьте файлы проекта.

---
+++ 2.12.cpp
@@ -0,0 +1,22 @@
+﻿// 2.12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "ru");
+    int k;
+    bool n = true;
+    bool z = false;
+    int x, y;
+    cout << "Введите икс и игрик: \n";
+    cin >> x >> y;
+    k = pow(x, 2) + pow(y, 2);
+    if (k <= 4) {
+        cout << "Значение логического выражения: " << n << endl;
+    }
+    else
+        cout << "Значение логического выражения: " << z << endl;
+}
